c8a8513a126ac260e672126bf24c2e72c6c42025
Eduardodsb <eduardo_b_bosa@hotmail.com>
2019-10-12 18:46:31 -0300

    +SplashScreen, Login

---
+++ MinervaPets/app/src/main/java/ufrj/devmob/minervapets/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package ufrj.devmob.minervapets;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ufrj.devmob.minervapets.ui.login.LoginActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        }, 5000);
+
+    }
+}
